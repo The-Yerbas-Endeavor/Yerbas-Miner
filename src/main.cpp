4fb2aae91451e85a688ed242756c30ee249f8e94
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-11 21:47:24 -0700

    feat: load miner config from file and cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
+#include "config.h"
 #include "miner.h"
 
 #include <exception>
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
     try {
-        yerbas::Miner miner;
+        const auto config = yerbas::load_config(argc, argv);
+        yerbas::Miner miner(config);
         return miner.run();
     } catch (const std::exception& e) {
         std::cerr << "Fatal: " << e.what() << '\n';
