d5f6f267198597e25a885d250bfad36017dc642b
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-11 21:47:30 -0700

    feat: pass app config into miner

--- src/miner.h
+++ src/miner.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include "config.h"
+
 namespace yerbas {
 
 class Miner {
 public:
+    explicit Miner(AppConfig config);
     int run();
+
+private:
+    AppConfig config_;
 };
 
 } // namespace yerbas
