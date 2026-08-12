a5d0b6f3942cd2f7376fe91fdc92bf16d568dff9
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-11 21:46:22 -0700

    feat: add miner configuration model

---
+++ src/config.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace yerbas {
+
+struct PoolConfig {
+    std::string url;
+    std::string user;
+    std::string password{"x"};
+};
+
+struct MinerConfig {
+    std::string worker{"rig1"};
+    unsigned int threads{0};
+};
+
+struct GpuConfig {
+    bool enabled{true};
+    std::vector<int> devices{0};
+    int intensity{0};
+};
+
+struct LoggingConfig {
+    std::string level{"info"};
+};
+
+struct AppConfig {
+    PoolConfig pool;
+    MinerConfig miner;
+    GpuConfig gpu;
+    LoggingConfig logging;
+    std::string config_path{"config.json"};
+};
+
+AppConfig load_config(int argc, char** argv);
+void print_config_help(const char* program);
+
+} // namespace yerbas
