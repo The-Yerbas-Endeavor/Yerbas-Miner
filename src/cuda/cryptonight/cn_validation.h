6f3816712566b7b53039f969322cdcd282e93dd1
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-19 21:45:25 -0700

    CUDA: expose CryptoNight validation kernel timing

--- src/cuda/cryptonight/cn_validation.h
+++ src/cuda/cryptonight/cn_validation.h
@@ -11,6 +11,7 @@ using Hash256 = std::array<std::uint8_t, 32>;
 Hash256 validation_hash(int device_id,
                         std::uint8_t variant,
                         const std::uint8_t* input,
-                        std::size_t length);
+                        std::size_t length,
+                        float* kernel_ms = nullptr);
 
 } // namespace yerbas::cuda::cryptonight
