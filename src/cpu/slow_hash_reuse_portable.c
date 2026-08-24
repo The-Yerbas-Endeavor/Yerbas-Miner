301cee46cdf9e5dba52a01a1f1d97378c300b4cf
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-24 13:33:06 -0700

    CPU: add portable CryptoNight multiversion object

---
+++ src/cpu/slow_hash_reuse_portable.c
@@ -0,0 +1,5 @@
+/* Portable CryptoNight implementation used by the runtime dispatcher. */
+#define yerbas_cn_reuse_backend yerbas_cn_reuse_backend_portable
+#define yerbas_cn_reuse_compile_features yerbas_cn_reuse_compile_features_portable
+#define yerbas_cn_slow_hash_reuse yerbas_cn_slow_hash_reuse_portable
+#include "slow_hash_reuse.c"
