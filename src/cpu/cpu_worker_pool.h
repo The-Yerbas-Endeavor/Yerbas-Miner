9d3edf4932dc7e03697de370842e6b073cfd523e
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-24 17:52:36 -0700

    CPU: allow production worker pool to use qualified lane width

--- src/cpu/cpu_worker_pool.h
+++ src/cpu/cpu_worker_pool.h
@@ -13,13 +13,14 @@ struct Candidate {
 
 class WorkerPool {
 public:
-    explicit WorkerPool(unsigned int thread_count);
+    explicit WorkerPool(unsigned int thread_count, unsigned int lane_width = 1);
     ~WorkerPool();
 
     WorkerPool(const WorkerPool&) = delete;
     WorkerPool& operator=(const WorkerPool&) = delete;
 
     unsigned int thread_count() const noexcept;
+    unsigned int lane_width() const noexcept;
 
     std::vector<Candidate> run(const std::array<std::uint8_t, 80>& base_header,
                                const std::array<std::uint8_t, 32>& target_le,
