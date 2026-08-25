04d6d1284cac5770171bb3723d89d3fab58df2ab
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-24 17:50:50 -0700

    CPU: benchmark lane scheduler at production batch size

--- src/cpu/cpu_lane_scheduler_tune.h
+++ src/cpu/cpu_lane_scheduler_tune.h
@@ -7,12 +7,14 @@ namespace yerbas::cpu {
 struct LaneSchedulerTuneResult {
     unsigned int workers{1};
     unsigned int lanes{1};
+    unsigned int batch{1};
     double throughput_hps{0.0};
     bool from_cache{false};
 };
 
 LaneSchedulerTuneResult tune_lane_scheduler(unsigned int hardware_threads,
                                             unsigned int configured_threads,
+                                            unsigned int production_batch,
                                             const std::string& mode);
 
 } // namespace yerbas::cpu
