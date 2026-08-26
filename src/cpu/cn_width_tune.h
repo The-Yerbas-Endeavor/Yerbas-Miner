5972f9e7f69cfbcabf2df40291a09b8f0b8e36c4
The-Yerbas-Endeavor <104656608+The-Yerbas-Endeavor@users.noreply.github.com>
2026-08-25 22:44:35 -0700

    CPU: retire old per-CN width tuner header

--- src/cpu/cn_width_tune.h
+++ src/cpu/cn_width_tune.h
@@ -1,19 +1,3 @@
 #pragma once
 
-#include <array>
-#include <string>
-
-namespace yerbas::cpu {
-
-struct CnWidthTuneResult {
-    std::array<unsigned int, 6> widths{{1,1,1,1,1,1}};
-    bool from_cache{false};
-};
-
-CnWidthTuneResult qualify_cn_widths(const std::string& mode);
-
-// Returns the width plan selected/loaded during startup qualification.
-// Defaults to all 1-way when tuning is off or has not run.
-std::array<unsigned int, 6> active_cn_widths() noexcept;
-
-} // namespace yerbas::cpu
+// Retired compatibility header. CPU tuning is unified in cpu_autotune.h.
